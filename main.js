67beed3104a778b56b68020755f84052e7a56baa
Dhruvil0208 <chaudharydhruvil671@gmail.com>
2022-12-25 21:33:52 +0530

    contact details addition

--- main.js
+++ main.js
@@ -4,16 +4,16 @@ function resetFunction() {
 
 
 
-    const form = document.getElementById("mainform");
+const form = document.getElementById("mainform");
 
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const fd = new FormData(form);
-        const entries = fd.entries();
-        const data = Object.fromEntries(entries);
-        console.log(data)
-        
-    })
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const fd = new FormData(form);
+    const entries = fd.entries();
+    const data = Object.fromEntries(entries);
+    console.log(data)
+    
+})
 
 
 
@@ -46,16 +46,107 @@ document.querySelector('.toggle').onclick = function()
 
 var coll = document.getElementsByClassName("other-details-header");
 var i;
+var chevron = document.getElementById("chevron")
 
 for (i = 0; i < coll.length; i++) {
-console.log(i);
+
   coll[i].addEventListener("click", function() {
     
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
+      chevron.style.transform = "rotate(360deg)"
     } else {
       content.style.display = "block";
+      chevron.style.transform = "rotate(90deg)";
     }
   });
+}
+
+
+const contactFormHTML = `<div class="address1">
+<fieldset>
+    <legend>
+        <select name="" id="address-option">
+            <option value="">Home</option>
+            <option value="">Work</option>
+        </select>
+    </legend>
+    <div class="address-field">
+        <div class="address-field-header">
+            <h3>Address</h3><i class="fa-solid fa-trash"></i>
+        </div>
+        <div class="address-field-container">
+           <div class="address-street">
+                <label for="street">Street</label>
+                <input type="text" name="street" id="street">
+           </div>
+           <div class="address-rest">
+                <div>
+                    <label for="zip">Zip</label>
+                    <input type="text" name="zip" id="zip">
+                </div>
+                <div>
+                    <label for="city">City</label>
+                    <input type="text" name="city" id="city">
+                </div>
+                <div id="state">
+                    <label for="state">State</label>
+                    <select name="state" id="state">
+                        <option value="Washington">Washington</option>
+                        <option value="Illoniss">Illoniss</option>
+                    </select>
+                </div>
+                <div >
+                    <label for="Country">Country</label>
+                    <select name="country" id="country">
+                        <option value="US">US</option>
+                        <option value="India">India</option>
+                    </select>
+                </div>
+                <div>
+                    <label for=""><i class="fa-solid fa-trash"></i></label>
+                </div>
+           </div>
+        </div>  
+    </div>
+    <div class="phone">
+        <h3>Phone</h3><i class="fa-solid fa-circle-plus"></i>
+        <div class="phone-header">
+            <p class="type">Type</p>
+            <p class="code">Code</p>
+            <p class="number">Number</p>
+        </div>
+        <hr>
+        <div class="phone-inputs">
+            <select name="type" id="type" class="type-input">
+                <option value="Cell">Cell</option>
+                <option value="Landline">Landline</option>
+            </select>
+            <select name="code" id="code" class="code-input">
+                <option value="+1">+1</option>
+                <option value="+91">+91</option>
+            </select>
+            <input type="text" name="number" id="number" class="number-input">
+        </div>
+    </div>
+    <div class="fax">
+        <h3>Fax</h3><i class="fa-solid fa-circle-plus"></i>
+    </div>
+    <div class="email">
+        <h3>Email</h3><i class="fa-solid fa-circle-plus"></i>
+    </div>
+    <div class="website">
+        <h3>Website</h3><i class="fa-solid fa-circle-plus"></i>
+    </div>
+</fieldset>
+</div>`
+
+function addContactField (){
+
+  document.getElementById("address-container").innerHTML += contactFormHTML;
+}
+
+function removeContactForm(deleteBtn){
+  deleteBtn.closest('.contact-detail-form').remove();
 }
